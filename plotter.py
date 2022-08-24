53c2f5ba0437b5d58c2609602713bfc5133a94e2
Lily <zx.kaze@gmail.com>
2022-08-24 00:24:26 -0400

    alternate ground plot calculations

--- plotter.py
+++ plotter.py
@@ -59,28 +59,28 @@ angular_element = angular_element-1
 #     x_lux_list.append(y_lux_list)
 
 #calculate lux at y for x distance for all vertical degrees (-88 to 88) by flattening of values to 1 plane (averaged)
-y_counter = 0
-x_lux_list = []
-for x in range(x_range):
-    x= x+1
-    x = x * increments
-    y_lux_list = []
-    for y_counter in range(len(y_axis_list)):
-        angle = -90+y_counter*interval+tilt_adjust
-        if angle >-90 and angle<90:
-            #candela = y_axis_list[y_counter][angular_element]
-            candela = 0
-            for candela_element in y_axis_list[y_counter]:
-                candela = candela + int(candela_element)
-            candela = candela/90
-            y = math.tan(angle * math.pi / 180) * x
-            d = math.sqrt(x**2+y**2)
-            if angle==0:
-                lux = (float(candela)*candela_to_lux_modifier)/(x**2)
-            else:
-                lux = (float(candela)*candela_to_lux_modifier)/(d**2)
-            y_lux_list.append((y,lux))
-    x_lux_list.append(y_lux_list)
+# y_counter = 0
+# x_lux_list = []
+# for x in range(x_range):
+#     x= x+1
+#     x = x * increments
+#     y_lux_list = []
+#     for y_counter in range(len(y_axis_list)):
+#         angle = -90+y_counter*interval+tilt_adjust
+#         if angle >-90 and angle<90:
+#             #candela = y_axis_list[y_counter][angular_element]
+#             candela = 0
+#             for candela_element in y_axis_list[y_counter]:
+#                 candela = candela + int(candela_element)
+#             candela = candela/90
+#             y = math.tan(angle * math.pi / 180) * x
+#             d = math.sqrt(x**2+y**2)
+#             if angle==0:
+#                 lux = (float(candela)*candela_to_lux_modifier)/(x**2)
+#             else:
+#                 lux = (float(candela)*candela_to_lux_modifier)/(d**2)
+#             y_lux_list.append((y,lux))
+#     x_lux_list.append(y_lux_list)
 
 #calculate lux at y for x distance for all horizontal degrees (-88 to 88) by angular slice
 # y_counter = 0
@@ -177,63 +177,83 @@ for x in range(x_range):
 #     y_counter+=1
     
 #reverse ground project calculations
-# height = 1
-# y_tilt = 0
-# x_tilt = 0
+height = 8
+y_tilt = 0
+x_tilt = 0
 
-# increments = 2
-# x_distance = 300
-# x_range = int(x_distance/increments)
-# z_distance = 150
-# z_range = int(z_distance/increments)*2
+increments = 1
+x_distance = 300
+x_range = int(x_distance/increments)
+z_distance = 150
+z_range = int(z_distance/increments)*2
 
-# angle_increment = 2
-# element_count = len(y_axis_list)
-# projection_list = []
+# angle_increment = 1
+element_count = len(y_axis_list)
+projection_list = []
 
-# for x in range(1,x_range-1):
-#     x=x*increments
-#     for z in range(z_range):
-#         z=-z_distance+z*increments
-#         #print(x,z)
-#         theta_x = math.degrees(math.atan(z/x))-x_tilt
-#         theta_y = math.degrees(math.atan(x/height))-90-y_tilt
-#         #print(theta_x,theta_y)
-#         #find x element
-#         x_th = 0
-#         y_th = 0
-#         for xth in range(element_count):
-#             angle = -90 + xth*2
-#             #print(angle)
-#             if theta_x < 0:
-#                 if theta_x < angle and angle<=0:
-#                     x_th = xth
-#                     #print("x - negative",angle,theta_x, -90+x_th*2)
-#                     break
-#             else:
-#                 if theta_x < angle and angle>=0:
-#                     x_th = xth
-#                     #print("x - positive",angle,theta_x,-90+x_th*2)
-#                     break
-#             for yth in range(element_count):
-#                 angle = -90 + yth*2
-#                 #print(angle)
-#                 if theta_y < 0:
-#                     if theta_y < angle and angle<=0:
-#                         y_th = yth
-#                         #print("y - negative",angle,theta_y, -90+y_th*2)
-#                         break
-#                 else:
-#                     if theta_y < angle and angle>=0:
-#                         y_th = yth
-#                         #print("y - positive",angle,theta_y,-90+y_th*2)
-#                         break
-#         candela = y_axis_list[y_th][x_th]
-#         d = math.sqrt((height**2)+(x**2)+(z**2))
-#         lux = (float(candela)*candela_to_lux_modifier)/(d**2)
-#         # if z ==0:
-#         #     print("calculated theta:",theta_x,theta_y,"list theta",-90+x_th*2,-90+y_th*2,"z,x",z,x,"candela",candela,d,lux)
-#         projection_list.append((z,x,lux))
+for x in range(1,x_range+1):
+    x=x*increments
+    for z in range(z_range):
+        z=-z_distance+z*increments
+        #print(x,z)
+        theta_x = math.degrees(math.atan(z/x))-x_tilt
+        
+        #theoretical angles
+        theta_y = -90+math.degrees(math.atan(x/height))-y_tilt
+        
+        #seems more accurate
+        # theta_y = math.degrees(math.atan(1-x/height))-y_tilt
+        # if theta_y<0:
+        #     theta_y = - 90 - theta_y
+        # else:
+        #     theta_y = - 90 + theta_y
+            
+        # theta_y = math.degrees(math.atan(2-x/height))+y_tilt
+        # if theta_y<0:
+        #     theta_y = - 90 - theta_y
+        # else:
+        #     theta_y = - 90 + theta_y
+        
+        #these 2 are the same
+        #theta_y = -90-math.degrees(math.atan(1-x/height))-y_tilt
+        #theta_y = 90+math.degrees(math.atan(1-x/height))-y_tilt
+        
+        #print(theta_x,theta_y)
+        #find x element
+        x_th = 0
+        y_th = 0
+        for xth in range(element_count):
+            angle = -90 + xth*2
+            #print(angle)
+            if theta_x < 0:
+                if theta_x < angle and angle<=0:
+                    x_th = xth
+                    #print("x - negative",angle,theta_x, -90+x_th*2)
+                    break
+            else:
+                if theta_x <= angle and angle>=0:
+                    x_th = xth
+                    #print("x - positive",angle,theta_x,-90+x_th*2)
+                    break
+            for yth in range(element_count):
+                angle = -90 + yth*2
+                #print(angle)
+                if theta_y < 0:
+                    if theta_y < angle and angle<=0:
+                        y_th = yth
+                        #print("y - negative",angle,theta_y, -90+y_th*2)
+                        break
+                else:
+                    if theta_y < angle and angle>=0:
+                        y_th = yth
+                        #print("y - positive",angle,theta_y,-90+y_th*2)
+                        break
+        candela = y_axis_list[y_th][x_th]
+        d = math.sqrt((height**2)+(x**2)+(z**2))
+        lux = (float(candela)*candela_to_lux_modifier)/(d**2)
+        if z ==0 and x%25==0:
+            print("calculated theta:",theta_x,theta_y,"list theta",-90+x_th*2,-90+y_th*2,"z,x",z,x,"candela",candela,d,lux)
+        projection_list.append((z,x,lux))
 
 
 class LuxPlotter(QtWidgets.QWidget):
@@ -243,9 +263,9 @@ class LuxPlotter(QtWidgets.QWidget):
         self.height=900
         self.width=900
         layout = QtWidgets.QVBoxLayout()
-        renderer = RenderPlane(self)
+        #renderer = RenderPlane(self)
         #renderer = RenderWallProjection(self)
-        #renderer = RenderGroundProjection(self)
+        renderer = RenderGroundProjection(self)
         layout.addWidget(renderer)
         self.setLayout(layout)
         self.setGeometry(0, 0, self.width, self.height)
@@ -454,7 +474,7 @@ class RenderGroundProjection(QtWidgets.QWidget):
         #canvas = QtGui.QPixmap(400,300)
         #self.label.setPixmap(canvas)
         # self.width = parent.geometry().width()
-        self.scale =1.25
+        self.scale = 1.25
         self.offsetx =50
         self.offsety = 50
         self.xrange = 300
@@ -476,7 +496,7 @@ class RenderGroundProjection(QtWidgets.QWidget):
             y = lux_item[1]
             #lux = lux_item[2]
             if y < self.yrange and y > -self.yrange and x<self.xrange and x>-self.xrange:
-                if lux_item[2]>0 and lux_item[2] < 25:
+                if lux_item[2]>0.4 and lux_item[2] < 25:
                     pen = QtGui.QPen(QtGui.QColor(0,0,int((lux_item[2]/25)*255)),pixel_size)
                 elif lux_item[2] >25 and lux_item[2] < 50:
                     pen = QtGui.QPen(QtGui.QColor(0,int((lux_item[2]/50)*255),0),pixel_size)
